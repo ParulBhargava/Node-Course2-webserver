725ff2cae0c984a3bf38826fa6ae10223a996094
ParulBhargava <paarulbhargava@gmail.com>
2019-01-22 15:26:40 +0530

    Projects page added

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require ("express");
 const hbs= require("hbs");
-
 const port=process.env.PORT || 4000;
-
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
@@ -12,16 +10,31 @@ hbs.registerHelper('currentYear', ()=>{
 })
 app.use(express.static(__dirname+'/public'));
 
-app.get('/home', (req,res)=>{
-    res.render('home.hbs',{
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
         pageTitle:'Home Page',
         headerTitle:'Welcome to home page'
     });
-    // res.send({
+    
+    // res.send("Home",{
     //     error:"Not connected"
     // });
 });
 
+app.get('/projects', (req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle:'My Project Case Studies',
+        headerTitle:'Welcome to my Profile page'
+    });
+});
+
+app.get('/about', (req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        headerTitle:'Welcome to About page'
+    });
+});
+
 app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is now running on port ${port}`);
 });
